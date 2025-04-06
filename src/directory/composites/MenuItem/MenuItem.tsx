836dbc08ebe457d76172d79d67c96825c5f5ae6f
haphuong96 <phuongha96@gmail.com>
2025-04-06 13:14:03 +0700

    add TODO: menu item

---
+++ src/directory/composites/MenuItem/MenuItem.tsx
@@ -0,0 +1,10 @@
+//TODO: Create a menu item component
+// mocked data:
+// {
+//     "id": 14,
+//     "name": "Slices of smoked turkey",
+//     "price": 100.0,
+//     "current_price": 50.0,
+//     "is_best_seller": true,
+//     "thumbnail": null
+// }
