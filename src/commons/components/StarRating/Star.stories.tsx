dfa96d5222a5e6e02adad709c373817c34c89b1d
haphuong96 <phuongha96@gmail.com>
2025-05-02 00:57:34 +0700

    fix: Star story

--- src/commons/components/StarRating/Star.stories.tsx
+++ src/commons/components/StarRating/Star.stories.tsx
@@ -5,7 +5,6 @@ const meta = {
   component: Star,
   title: "Commons/Components/Star",
   argTypes: {
-    id: { control: { type: "number", min: 0, max: 10 } },
     fillPercentage: { control: { type: "number", min: 0, max: 100, step: 1 } },
     size: { control: { type: "number", min: 8, max: 64, step: 1 } },
     color: { control: { type: "color" } },
@@ -19,28 +18,24 @@ type Story = StoryObj<typeof meta>;
 
 export const Empty: Story = {
   args: {
-    id: 0,
     fillPercentage: 0,
   },
 };
 
 export const Half: Story = {
   args: {
-    id: 1,
     fillPercentage: 50,
   },
 };
 
 export const Full: Story = {
   args: {
-    id: 2,
     fillPercentage: 100,
   },
 };
 
 export const CustomSizeAndColor: Story = {
   args: {
-    id: 3,
     fillPercentage: 100,
     size: 32,
     color: "#00BFFF",
